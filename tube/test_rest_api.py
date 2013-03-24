e628390b2b0f61335bd4637d0f4908b8032d1a30
chrishaum <chrishaum@gmail.com>
2013-03-24 03:27:58 -0400

    added rest api tests

---
+++ tube/test_rest_api.py
@@ -0,0 +1,22 @@
+from nose.tools import *
+from fabric.api import env, local, run
+from requests import put, get
+
+def test_404():
+	r = get('http://localhost:5000/FAIL')
+	eq_(r.status_code, 404)
+
+def list(order, direction):
+	r = get('http://localhost:5000/api/list', params={'direction':direction, 'order': order})
+	return r.json()
+
+
+def test_list():
+	j = list('name','ascending')
+	eq_(j[0]['name'],'vid0')
+
+	j = list('timestamp','descending')
+	assert j[0]['timestamp'] > j[1]['timestamp']
+
+	j = list('rating','ascending')
+	assert j[0]['rating'] < j[1]['rating']
