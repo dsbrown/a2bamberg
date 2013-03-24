f33bcdbb591bdd905a422356eddf500044a9430e
chrishaum <chrishaum@gmail.com>
2013-03-24 05:31:17 -0400

    completed /api/upload endpoint

--- tube/api.py
+++ tube/api.py
@@ -1,22 +1,26 @@
 # See http://httpstatus.es/ for HTTP response codes
 from pprint import pprint
-from flask import Flask, request
+from flask import Flask, request, jsonify, redirect
 from flask.ext.restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-# Test data
 vids = []
-for i in range(3):
-	vids.append({	
-	'name': 'vid{}'.format(i),
-	'timestamp': 10 - i,
-	'rating': i,
-	'num_ratings': 22,
-	's3_url': 'http://s3....',
-	'streaming_url': 'http://cloudfront...',
-})
+def save_video(name, s3_url):
+	vids.append({
+		'n': len(vids) + 1,
+		'name': name,
+		'timestamp': 'NEEDS_TIMESTAMP',
+		'rating': 0,
+		'num_ratings': 0,
+		's3_url': s3_url,
+		'streaming_url': 'NEEDS_CLOUDFRONT'
+		})
+
+# Test data
+save_video('kitten-surprise.mp4', 'https://s3.amazonaws.com/chrishaum.bucket/uploads/kitten-surprise.mp4')
+save_video('talking-cats', 'https://s3.amazonaws.com/chrishaum.bucket/uploads/talking-cats.mp4')
 
 # Parse request arguments
 parser = reqparse.RequestParser()
@@ -33,7 +37,7 @@ reverse = {'ascending': False, 'descending': True}
 def validate_args(args):
 	'''Validate request arguments'''
 	if args['order'] not in valid_keys:
-		abort(400, message=u"Invalid 'order' value: '{}'. Valid values: {}".format(args['order'], valid_keys))
+		abort(400, message=u"Invalid 'order' value: {}. Valid values: {}".format(args['order'], valid_keys))
 	if args['direction'] not in valid_directions:
 		abort(400, message=u"Invalid 'direction' value: {}. Valid values: {}".format(args['direction'], valid_directions))
 
@@ -61,6 +65,15 @@ api.add_resource(List, '/api/list')
 api.add_resource(Upload, '/api/upload')
 api.add_resource(Delete, '/api/delete')
 api.add_resource(Rate, '/api/rate')
+
+@app.route('/upload/success')
+def upload_success():
+	bucket = request.args.get('bucket','')
+	key = request.args.get('key','')
+	url = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
+	save_video(name=key, s3_url=url)
+	return redirect('/api/list?order=rating&direction=descending')
+
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', debug=True)
